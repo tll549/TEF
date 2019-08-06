e57b65187fc42107a0cb2b49c0b80fb840e06ecf
tll549 <tll549@yahoo.com.tw>
2019-08-05 20:58:36 -0500

    'init'

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TEF",
+    version="0.0.1",
+    author="tll549",
+    author_email="el@tll.tl",
+    description="Ethan (tll549)'s Exploratory Functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
