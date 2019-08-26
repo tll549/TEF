d6112bb3484ce4858b7394f8f49208e75e809a70
tll549 <tll549@yahoo.com.tw>
2019-08-25 21:39:17 -0500

    0.5.5

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 import setuptools
 
+import re
+import sys
+
+with open('TEF/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            _, _, __version__ = line.replace("'", '').split()
+            break
+
 # with open("README.md", "r") as fh:
 #     long_description = fh.read()
 long_description = "See https://github.com/tll549/TEF for more details"
 
 setuptools.setup(
     name="TEF",
-    version="0.5.1",
+    version=__version__,
     author="tll549",
     author_email="el@tll.tl",
-    description="Ethan (tll549)'s Exploratory Functions",
+    description="tll549's Exploratory Functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tll549/TEF",
